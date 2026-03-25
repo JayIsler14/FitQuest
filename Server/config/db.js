25edb220890104cdc2d7b91bca7346f5cdad59c0
JayIsler14 <157390822+JayIsler14@users.noreply.github.com>
2026-03-24 22:50:23 -0400

    Delete Server directory

--- Server/config/db.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,  // 🔹 just reference the env variable
-  ssl: { rejectUnauthorized: false }          // required for Neon
-});
-
-module.exports = pool;
